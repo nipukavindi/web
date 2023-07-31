9fcfc665bec61131fb0cd15b0d028e9aceb3df54
nipukavindi <141056729+nipukavindi@users.noreply.github.com>
2023-07-31 21:44:20 +0500

    Add files via upload

---
+++ la10/Form.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title> NSBM  </title>
+<?php
+$h=$_REQUEST["fn"];
+   echo ("First Name is : $h<br>"); 
+$t=$_REQUEST["la"];
+   echo ("Last Name is : $t<br>"); 
+$a=$_REQUEST["un"];
+   echo ("User Name is : $a<br>"); 
+$b=$_REQUEST["password"];
+   echo ("password is : $b<br>"); 
+$c=$_REQUEST["na"];
+   echo ("Gender is : $c<br>"); 
+
+$q=$_REQUEST["email"];
+   echo ("Email is : $q<br>"); 
+$w=$_REQUEST["la"];
+   echo ("phone Number is : $w<br>"); 
+    
+?>
+</head>
+    <body>
+    </body>
+    </html>
